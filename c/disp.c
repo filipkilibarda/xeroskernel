0bc9823332f02ee612ca6d225fbe2ec63b9c90bc
Filip Kilibarda <filipcsc@gmail.com>
2019-04-04 15:01:34 -0700

    Added a TODO to dispatcher

--- c/disp.c
+++ c/disp.c
@@ -146,6 +146,15 @@ extern void dispatch(void) {
                 }
 
                 if (process->state != PROC_STOPPED) {
+                    // TODO: This is weird. We're essentially saying,
+                    //     "if a STOPPED process made a sysstop call"
+                    //  then don't enqueue it in the ready queue.
+                    //  That makes sense, but how can a STOPPED process be
+                    //  making a system call in the first place?
+                    //  Seems like we should actually have a check at the top
+                    //  of the dispatcher loop, checking that the current
+                    //  process is not STOPPED, because it makes no sense for
+                    //  a STOPPED process to be in the dispatch loop?
                     enqueue_in_ready(process);
                 }
                 process = dequeue_from_ready();
